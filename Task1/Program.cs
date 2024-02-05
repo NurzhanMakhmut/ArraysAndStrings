f3049639d50ee617bf9a071331481bb16dc26362
Nurzhan_Makhmut <87759046037b@gmail.com>
2024-02-06 01:21:52 +0600

    3 tasks

---
+++ Task1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задайте двумерный массив символов (тип char
+// [,]). Создать строку из символов этого массива. 
+string GetStringFromCharArray (char[,] array)
+{
+    string result = string.Empty;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            result += array[i,j]; 
+        }
+    }
+    return result;
+}
+
+char [,] chars  =  {
+    {'a', 'b', 'c'},
+    {'d', 'e', 'f'}
+};
+
+string res = GetStringFromCharArray(chars);
+Console.WriteLine($"\"{res}\"");
---
+++ Task3/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задайте произвольную строку. Выясните,
+// является ли она палиндромом
+
+bool CheckPalindrome(string str)
+{
+    for (int i = 0, j = str.Length - 1; i < str.Length / 2; i++,j--)
+        {
+            if (str[i] != str[j])
+            {
+                return false;
+            }
+        }
+    return true;
+}
+Console.Write("Введите строку на проверку:");
+string str = Console.ReadLine();
+str = str.ToLower();
+bool isPalindrome = CheckPalindrome(str);
+
+if (isPalindrome)
+{
+    Console.WriteLine("ДА строчка является полиндромом");
+}
+else
+{
+    Console.WriteLine("Нет, строчка не является полиндромом");
+}
